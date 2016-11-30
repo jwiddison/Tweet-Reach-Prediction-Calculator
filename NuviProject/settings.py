72f4df8e860dc0074555357a7931eb06d407c204
root <root@ip-172-31-41-249.us-west-2.compute.internal>
2016-11-30 20:18:15 +0000

    Added '*' to allowed hosts.

--- NuviProject/settings.py
+++ NuviProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '15ov^j4kz7&gdj$ss*96z=r^46w*h*0dyd%j_-al^x%throu@v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # AUTH_USER_MODEL = 'account.User'
 
